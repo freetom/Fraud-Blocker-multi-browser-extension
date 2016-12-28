70bfd1be2ac4253aff2828854501e2db8e0fec38
tomas <tomasbortoli@gmail.com>
2016-12-28 18:04:06 +0100

    v0.4.4
    background.js: graphic layout fix, the text on the button was not
    rendered correctly in all the cases.
    
    chrome.patch: updated accordingly, plus, chrome has one rendering case
    more
    
    Now "text on button" rendering works in any use case combination thanks
    to the combination of the events used.

--- background.js
+++ background.js
@@ -1,20 +1,25 @@
 //setup message handler for requests coming from popup (toolbar button) and content_scripts
 chrome.runtime.onMessage.addListener(messageHandler);
 
-//setup handler to update browser action's badge text
-chrome.tabs.onActivated.addListener(function(activeInfo){
-  chrome.tabs.executeScript(null,{file:"content_script.js"});
-  
+
+function checkTab(info){
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var res = getStatus(tabs[0].url);
     if(res.msg=="grey"){
       var nFraud=res.nFraud;
       if(isNaN(nFraud)) //sanitize
         nFraud='error';
-      chrome.browserAction.setBadgeText({tabId: activeInfo.tabId, text: nFraud});
-      chrome.browserAction.setBadgeBackgroundColor({tabId: activeInfo.tabId, color: "#cc0000"});
+      chrome.browserAction.setBadgeText({tabId: tabs[0].tabId, text: nFraud});
+      chrome.browserAction.setBadgeBackgroundColor({tabId: tabs[0].tabId, color: "#cc0000"});
     }
+    else
+      chrome.browserAction.setBadgeText({tabId: tabs[0].tabId, text: ""});
   });
-});
+}
+
+//setup handler to update browser action's badge text
+chrome.tabs.onActivated.addListener(checkTab);
+chrome.tabs.onUpdated.addListener(checkTab);
+
 
 storage.get(null,init);
