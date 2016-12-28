47a8a3ebb006cde75c2f527be9fc635eea8e2ec8
tomas <tomasbortoli@gmail.com>
2016-12-28 21:02:52 +0100

    v0.4.5
    /include/sync.js: more efficient. Now cover also localList and localConlist. Now syncing is complete.

--- include/sync.js
+++ include/sync.js
@@ -6,26 +6,33 @@ function syncLists(){
 /*	Sync the lists with the following logic
  *	If a value is in whitelist then it can't be on blacklist or greylist
  *	If a value is in blacklist then it can't be in greylist
+ *	If a value is in whitelist or blacklist it can't be in localList & localConList
 */
 const syncDelay=3600000*4;	//4 hours is good
 function syncHelper(){
 	//every 4 hours, sync lists
 	if(strToDate(getTimeNormalized())-strToDate(lastSyncTimestamp)>syncDelay){
 		try{
-			for(var site in whiteList){
-				if(blackList[site]!=null)
+			for(var site in blackList){
+				if(whiteList[site]!=null)
 					delete blackList[site];
 				if(greyList[site]!=null)
 					delete greyList[site];
 			}
-			for(var site in blackList){
-				if(greyList[site]!=null)
-					delete greyList[site];
+			for(var site in localList){
+				if(blackList[site]!=null || whiteList[site]!=null)
+					delete localList[site];
+			}
+			for(var site in localConList){
+				if(blackList[site]!=null || whiteList[site]!=null)
+					delete localConList[site];
 			}
 			lastSyncTimestamp=getTimeNormalized();
 
 			storage.set({black:JSON.stringify(blackList)});
 			storage.set({grey:JSON.stringify(greyList)});
+			storage.set({local:JSON.stringify(localList)});
+			storage.set({localCon:JSON.stringify(localConList)});
 			storage.set({lastSync:lastSyncTimestamp});
 		}
 		catch(ex){}
